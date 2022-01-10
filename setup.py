57cfeb5b4299d144c56abfd3ba4b115981241646
Michel Fodje <michel4j@gmail.com>
2022-01-10 11:12:50 -0600

    Add setup, get_version and preparing for initial release

---
+++ setup.py
@@ -0,0 +1,33 @@
+
+from setuptools import setup, find_packages
+from szrpc import get_version
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='swift-rpc',
+    version=get_version(),
+    url="https://github.com/michel4j/gepics",
+    license='MIT',
+    author='Michel Fodje',
+    author_email='michel4j@gmail.com',
+    description='A simple Python RPC Library using ZeroMQ & MsgPack',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='rpc networking development',
+    packages=find_packages(),
+    install_requires=requirements + [
+        'importlib-metadata ~= 1.0 ; python_version < "3.8"',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ szrpc/__init__.py
@@ -0,0 +1,41 @@
+import os
+import re
+from subprocess import CalledProcessError, check_output
+
+
+PACKAGE_DIR = os.path.dirname(__file__)
+
+
+def get_version(prefix='v', package=PACKAGE_DIR):
+
+    # Return the version if it has been injected into the file by git-archive
+    tag_re = re.compile(rf'\btag: {prefix}([0-9][^,]*)\b')
+    version = tag_re.search('$Format:%D$')
+    name = __name__.split('.')[0]
+
+    if version:
+        return version.group(1)
+
+    package_dir = package
+    if os.path.isdir(os.path.join(package_dir, '.git')):
+        # Get the version using "git describe".
+        version_cmd = 'git describe --tags --abbrev=0'
+        release_cmd = 'git rev-list HEAD ^$(git describe --abbrev=0) | wc -l'
+        try:
+            version = check_output(version_cmd, shell=True).decode().strip()
+            release = check_output(release_cmd, shell=True).decode().strip()
+            return f'{version}.{release}'.strip(prefix)
+        except CalledProcessError:
+            version = '0.0'
+            release = 'dev'
+            return f'{version}.{release}'.strip(prefix)
+    else:
+        try:
+            from importlib import metadata
+        except ImportError:
+            # Running on pre-3.8 Python; use importlib-metadata package
+            import importlib_metadata as metadata
+
+        version = metadata.version(name)
+
+    return version
